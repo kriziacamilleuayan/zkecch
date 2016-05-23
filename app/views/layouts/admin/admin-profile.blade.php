aca33e04ec476deddb34748bcfd260af5f574a37
kcNeko <kriziacamilleuayan@gmail.com>
2016-05-23 22:58:35 +0800

    when the stars are gone, it means you are blind

---
+++ app/views/layouts/admin/admin-profile.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.artist.master')
+
+@section('title')
+Profile
+@endsection
+
+@section('content')
+<br><br>
+<h1>Profile</h1>
+<br><br>
+@if($user->strImagePath == NULL)
+<img src="img/signup.png" alt="profile Picture" style="width: 200px; height: 200px;">
+@else
+<img src="{{ asset($user->strImagePath) }}" alt="profile Picture" style="width: 200px; height: 200px;">
+@endif
+<br><br>
+<P>Name: <b>{{$user->strName}}</b></P>
+<p>Pen-Name: <b>{{$user->strPenName}}</b></p>
+<p>Email: <b>{{$user->strUserEmail}}</b></p>
+<p>Contact Number: <b>{{$user->strContactNumber}}</b></p>
+<p>Website: <b>{{$user->strWebsite}}</b></p>
+<p>Description/Bio: <b>{{$user->strDescription}}</b></p>
+<br><br><br>
+@endsection
--- app/views/layouts/admin/master.blade.php
+++ app/views/layouts/admin/master.blade.php
@@ -52,9 +52,9 @@
             </div>
     <div class="container">
         <ul class="main-nav">
-        	<li><a href="#header">{{ Session::get('name') }}(Admin)</a></li>
+        	<li><a href="{{URL::to('admin-profile')}}">{{ Session::get('name') }}(Admin)</a></li>
             <li><a href="{{URL::to('admin-artistaccounts')}}">Artist Accounts</a></li>
-            <li class="small-logo"><a href="#header"><img src="img/small-logo.png" alt=""></a></li>
+            <li class="small-logo"><a href="{{URL::to('admin-home')}}"><img src="img/small-logo.png" alt=""></a></li>
             <li><a href="#team">Artworks</a></li>
             <li><a href="{{URL::to('logout')}}">Logout</a></li>
         </ul>

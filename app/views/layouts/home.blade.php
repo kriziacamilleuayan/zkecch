9a752e60e4080144a74e5cf0c7c17b03a0cca69a
kcNeko <kriziacamilleuayan@gmail.com>
2016-05-23 17:59:37 +0800

    wiew search added

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -41,13 +41,25 @@
 
 <nav class="main-nav-outer" id="test"><!--main-nav-start-->
 	<div class="container">
+        <div id="custom-search-input" class="col-sm-2 pull-right" style="margin-top: 35px">
+                <div class="input-group">
+                    <input type="text" class="form-control input-sm" placeholder="Search" />
+                    <span class="input-group-btn">
+                        <button class="btn btn-info btn-sm" type="button" style="background-color: pink">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </div>
         <ul class="main-nav">
-        	<li><a href="#header">Home</a></li>
+        	<li></li>
+            <li><a href="#header">Home</a></li>
             <li><a href="#service">Services</a></li>
             <li><a href="#Portfolio">Portfolio</a></li>
             <li class="small-logo"><a href="#header"><img src="img/small-logo.png" alt=""></a></li>
             <li><a href="#team">Team</a></li>
             <li><a href="#contact">Contact</a></li>
+            
         </ul>
         <a class="res-nav_click" href="#"><i class="fa-bars"></i></a>
     </div>
